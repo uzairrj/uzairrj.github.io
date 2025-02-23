8e783ce00978c87fcfa5dca49b1544ca417de0eb
Uzair Khan <uzairrj@gmail.com>
2025-02-23 21:19:39 +0100

    Animated bloby background

---
+++ src/componenets/background/blob.tsx
@@ -0,0 +1,35 @@
+import React from "react"
+import "./blob.css"
+import { motion, useTime, useTransform } from "motion/react"
+
+type BlobStyle = {
+    style?: React.CSSProperties;
+}
+
+type BlobProps = {
+    blobs: BlobStyle[]
+    top?: number | string;
+    left?: number | string;
+    right?: number | string;
+    bottom?: number | string;
+}
+
+function Blob({blobs, top, left, bottom, right}: BlobProps) {
+    const time = useTime()
+
+    return (
+        <div className="blob-container" style={{top, left, bottom, right}}>
+                {blobs.map((blob, index) => {
+                    const randomNumber = Math.random()
+                    const rotate = useTransform(time, [0, 20000], [0, 200+(Math.floor(randomNumber)*360)], { clamp: false })
+                    const translateX = useTransform(time, t => Math.sin(t/((1000 + randomNumber * 400) + (randomNumber * Math.PI * 2 ))) * (20 + (randomNumber * 50)))
+                    const translateY = useTransform(time, t => Math.cos(t/(1000 + randomNumber * 400) ) + (randomNumber * Math.PI * 2 ) * (20+(randomNumber * 30)));
+                    return (
+                        <motion.div key={index} className="blob" style={{...blob.style, rotate ,x:translateX, y:translateY}} />
+                    )
+                })}
+        </div>
+    )
+}
+
+export {Blob}
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,64 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+import {Blob} from './componenets/background/blob'
+
+const blobGroup1 = [
+  {
+    style: {background: 'rgba(225, 93, 240, 0.7)', width: '40vw', height: '30vw'}
+  },
+  {
+    style: {background: 'rgba(228, 240, 93, 0.6)', width: '30vw', height: '15vw', transform: 'translate(10px,100px)'}
+  },
+  {
+    style: {background: 'rgba(250, 51, 36, 1)', width: '20vw', height: '30vw', transform: 'translate(100px,10px)'}
+  }
+]
+
+const blobGroup2 = [
+  {
+    style: {background: 'rgba(215, 134, 20, 0.7)', width: '40vw', height: '30vw'}
+  },
+  {
+    style: {background: 'rgba(93, 184, 240, 0.6)', width: '30vw', height: '15vw', transform: 'translate(10px,100px)'}
+  },
+  {
+    style: {background: 'rgb(13, 208, 26)', width: '20vw', height: '30vw', transform: 'translate(100px,10px)'}
+  }
+]
+
+const blobGroup3 = [
+  {
+    style: {background: 'rgba(20, 215, 134, 0.7)', width: '40vw', height: '30vw'}
+  },
+  {
+    style: {background: 'rgba(226, 64, 188, 0.6)', width: '30vw', height: '15vw', transform: 'translate(10px,100px)'}
+  },
+  {
+    style: {background: 'rgb(97, 13, 208)', width: '20vw', height: '30vw', transform: 'translate(100px,10px)'}
+  }
+]
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <>
+      <svg xmlns="http://www.w3.org/2000/svg">
+        <defs>
+          <filter id="goo">
+            <feGaussianBlur in="SourceGraphic" stdDeviation="10" result="blur" />
+            <feColorMatrix in="blur" mode="matrix" values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 18 -8" result="goo" />
+            <feBlend in="SourceGraphic" in2="goo" />
+          </filter>
+        </defs>
+      </svg>
+      <div className="blob-container-filter">
+        <Blob blobs={blobGroup1} top={'-10vh'} left={'-10vw'}/>
+        <Blob blobs={blobGroup2} bottom={'25vh'} left={'30vw'}/>
+        <Blob blobs={blobGroup3} right={'55vh'} top={'0vw'}/>
+      </div>
+      <div className="content">
+        <h1 className="title">Blobby Background</h1>
+      </div>
+    </>
   </StrictMode>,
 )
