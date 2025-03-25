6e371db5ee753a3af02d105a775f21ff4e4a9214
Uzair Khan <uzairrj@gmail.com>
2025-03-25 16:13:18 +0100

    Add link to source code in Footer component

--- src/componenets/text/footer.tsx
+++ src/componenets/text/footer.tsx
@@ -5,7 +5,7 @@ function Footer () {
     return (
         <div className="footer">
             <p>Designed and devloped by me.&nbsp;
-                <motion.a className="source-code"
+                <motion.a className="source-code" href="https://github.com/uzairrj/uzairrj.github.io" target="_blank"
                     whileHover={{ color: "rgb(255, 255, 255)" }}
                 >source code</motion.a></p>
         </div>
