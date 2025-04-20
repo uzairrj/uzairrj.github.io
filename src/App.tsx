9442a26b1d0d66064691d2354ecc81962cd184d6
Uzair Khan <uzairrj@gmail.com>
2025-04-20 12:55:42 +0200

    Refactor App component: remove redundant import and adjust layout styles

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import './App.css'
 import Footer from './componenets/text/footer'
 import NavBar from './componenets/miscellaneous/navBar'
 import Home from './pages/home'
-import { BrowserRouter as router, Routes, Route, BrowserRouter } from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Education from './pages/education'
 import Header from './componenets/miscellaneous/header'
 
@@ -10,10 +10,9 @@ function App() {
   return (
     <BrowserRouter>
       <div className="center-container">
-        <div style={{flex:1}}>
+        <div style={{flex:1, width:"100%"}}>
           <Routes>
             <Route path="/" element={<Home />} />
-            <Route path="/home" element={<Home />} />
             <Route path="/education" element={<Header><Education /></Header>} />
           </Routes>
         </div>
