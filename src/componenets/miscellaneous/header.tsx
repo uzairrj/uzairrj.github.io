f052b64d49f184ee10901e12e94ed119c780d79e
Uzair Khan <uzairrj@gmail.com>
2025-04-20 13:01:25 +0200

    Add header text styling and integrate into Header component

--- src/componenets/miscellaneous/header.tsx
+++ src/componenets/miscellaneous/header.tsx
@@ -6,6 +6,7 @@ function Header({children}: {children: React.ReactNode}) {
     return(
         <div className="header-container">
             <ProfilePicture imgUri={"/profilePicture.jpeg"} size="10vh" borderOnHover={false} customClass="header-picture"/>
+            <p className="header-text">Uzair Khan</p>
             <div style={{flex: 1, width: "100%"}}>
                 {children}
             </div>
