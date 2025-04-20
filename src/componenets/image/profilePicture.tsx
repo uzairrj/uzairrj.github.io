f18cfbad6662c885aa6908e7314f7df530129a42
Uzair Khan <uzairrj@gmail.com>
2025-04-20 12:24:25 +0200

    Refactor ProfilePicture component to accept dynamic size and custom styles

--- src/componenets/image/profilePicture.tsx
+++ src/componenets/image/profilePicture.tsx
@@ -1,18 +1,19 @@
-import { useEffect } from 'react'
 import './profilePicture.css'
 import {motion} from 'framer-motion'
 
 type PrimaryPictureProps = {
-    imgUri: string
+    imgUri: string,
+    borderOnHover?: boolean,
+    customClass?: string,
+    customStyle?: React.CSSProperties,
+    size?: string
 }
 
-function ProfilePicture({imgUri}:PrimaryPictureProps){
-    useEffect   (()=>{
-        console.log(imgUri)})
+function ProfilePicture({imgUri, size='25vh', borderOnHover=true, customClass='', customStyle={}}:PrimaryPictureProps){
     return (
-        <motion.div className="profile-image-container" 
-        whileInView={{width: "25vh", height: "25vh", transition: {duration: 1}}}
-        whileHover={{border: "3.5px white dashed", scale:1.1, transition: {duration: 0.7, type: "spring", stiffness: 120}}}
+        <motion.div className={`profile-image-container ${customClass}`} style={{...customStyle,  width: size, height: size}}
+        whileInView={{width: size, height: size, transition: {duration: 1}}}
+        whileHover={borderOnHover ? {border: "3.5px white dashed", scale:1.1, transition: {duration: 0.7, type: "spring", stiffness: 120}}: {}}
         >
             <img src={imgUri} className='profile-image' alt='profile'/>
         </motion.div> 
