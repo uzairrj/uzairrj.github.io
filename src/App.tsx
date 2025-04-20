2ed8d5ae7a65e41c32e3109f6e20b1281bc11a24
Uzair Khan <uzairrj@gmail.com>
2025-04-20 12:24:01 +0200

    Refactor routing in App component and adjust layout styles for NavBar, Footer, and Home page

--- src/App.tsx
+++ src/App.tsx
@@ -4,17 +4,20 @@ import NavBar from './componenets/miscellaneous/navBar'
 import Home from './pages/home'
 import { BrowserRouter as router, Routes, Route, BrowserRouter } from 'react-router-dom'
 import Education from './pages/education'
+import Header from './componenets/miscellaneous/header'
 
 function App() {
   return (
     <BrowserRouter>
       <div className="center-container">
+        <div style={{flex:1}}>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/home" element={<Home />} />
+            <Route path="/education" element={<Header><Education /></Header>} />
+          </Routes>
+        </div>
         <NavBar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/home" element={<Home />} />
-          <Route path="/education" element={<Education />} />
-        </Routes>
         <Footer />
       </div>
     </BrowserRouter>  
