a9e5009f3ca25f936ba1acac296ebec6811bbddc
Uzair Khan <uzairrj@gmail.com>
2025-06-30 21:33:16 +0200

    Making header independent

--- src/componenets/miscellaneous/header.tsx
+++ src/componenets/miscellaneous/header.tsx
@@ -2,14 +2,11 @@
 import ProfilePicture from "../image/profilePicture"
 import "./header.css"
 
-function Header({children}: {children: React.ReactNode}) {
+function Header() {
     return(
         <div className="header-container">
             <ProfilePicture imgUri={"/profilePicture.jpeg"} size="10vh" borderOnHover={false} customClass="header-picture"/>
             <p className="header-text">Uzair Khan</p>
-            <div style={{flex: 1, width: "100%"}}>
-                {children}
-            </div>
         </div>
     )
 }
