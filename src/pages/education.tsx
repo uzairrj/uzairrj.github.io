2bf69f3918d7b4dab27291069514287c33918ba3
Uzair Khan <uzairrj@gmail.com>
2025-06-30 22:59:34 +0200

    Add Education component with content and styling for education section

--- src/pages/education.tsx
+++ src/pages/education.tsx
@@ -1,9 +1,64 @@
+import './education.css'
+import { motion } from 'framer-motion';
+
+
+function EducationContent({title, institution, date, description}: {
+  title: string;
+  institution: string;
+  date: string;
+  description: string;
+}) {
+  return (
+    <div>
+       <h3 className='education-title'>{title}</h3>
+        <p className='education-institution'>{institution}</p>
+        <p className='education-date'>{date}</p>
+        <p className='education-description'>{description}</p>
+    </div>
+  )
+}
+
+function UnderlineLink( {href, text}: {
+  href: string;
+  text: string;
+}) {
+  const underlineVariants = {
+    hidden: { width: 0 },
+    visible: { width: "100%" }
+  };
+  return (
+    <motion.a
+      href={href}
+      className="education-navbar-link"
+      initial="hidden"
+      whileHover="visible"
+    >
+      {text}
+      <motion.span
+        className="underline"
+        variants={underlineVariants}
+        transition={{ duration: 0.3 }}
+      ></motion.span>
+    </motion.a>
+  )
+}
+
 
 function Education() {
   return (
-    <div>
-      <h1>Education</h1>
-      <p>Details about education will go here.</p>
+    <div className="education-container">
+      <div className="education-navbar">
+        <ul className="education-navbar-list">
+          <UnderlineLink href="#bachelors" text="Bachelors" />
+          <UnderlineLink href="#masters" text="Masters" />
+          <UnderlineLink href="#certifications" text="Ph. D" />
+        </ul>
+      </div>
+      <EducationContent 
+        title='Bachelors in Computer Science' 
+        institution='COMSATS University Islamabad' 
+        date='Spring 2016 - Spring 2019' 
+        description="Graduated with a Bachelor's degree in Computer Science, specializing in software development and data structures. Completed various projects including a web application for managing student records and a mobile app for task management." />
     </div>
   );
 }
